71543cb473ec038cdb0c02119243a8ca57fe50ab
rb <rkrb@tyfonmail.se>
2025-02-03 10:40:54 +0100

    init commit

---
+++ src/main/java/org/example/modbusbackend/ModbusBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.modbusbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModbusBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModbusBackendApplication.class, args);
+    }
+
+}
