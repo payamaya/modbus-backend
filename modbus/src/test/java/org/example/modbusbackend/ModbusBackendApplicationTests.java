0d0e11dbc2c3e8736080255e4afa98299f38bae4
rb <rkrb@tyfonmail.se>
2025-02-18 14:09:33 +0100

    test modbus connection

--- modbus/src/test/java/org/example/modbusbackend/ModbusBackendApplicationTests.java
+++ modbus/src/test/java/org/example/modbusbackend/ModbusBackendApplicationTests.java
@@ -1,13 +1,53 @@
 package org.example.modbusbackend;
 
+
+import com.ghgande.j2mod.modbus.io.ModbusTCPTransaction;
+import com.ghgande.j2mod.modbus.msg.ReadMultipleRegistersResponse;
+import com.ghgande.j2mod.modbus.net.TCPMasterConnection;
 import org.junit.jupiter.api.Test;
+
+import org.mockito.Mock;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+
+import java.net.InetAddress;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class ModbusBackendApplicationTests {
 
+
+
+    @Value("${modbus.slave.ip}")
+    private String slaveIp;
+    @Value("${modbus.slave.port}")
+    private int slavePort;
+    @Mock
+    private TCPMasterConnection connection;
+
+
+
     @Test
-    void contextLoads() {
+    void testShouldReturnSuccessfulConnectionToSpecificModbusSlave() throws Exception {
+        //Arrange
+        InetAddress address = InetAddress.getByName(slaveIp);
+        connection = new TCPMasterConnection(address);
+        connection.setPort(slavePort);
+
+        //Act
+        connection.connect();
+
+        //Assert
+        assertTrue(connection.isConnected());
+        connection.close();
+
     }
 
 }
