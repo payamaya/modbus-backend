57fbd1db47987a80182a55c61c43e851ec117170
Panas425 <anasabualsoud@hotmail.com>
2025-02-19 05:14:54 +0100

    reconstructed files

--- api/src/main/java/com/database/model/ModbusData.java
+++ api/src/main/java/com/database/model/ModbusData.java
@@ -1,4 +1,4 @@
-package com.model;
+package com.database.model;
 
 import jakarta.persistence.*;
 import lombok.*;
--- api/src/main/java/com/database/repository/ModbusDataRepository.java
+++ api/src/main/java/com/database/repository/ModbusDataRepository.java
@@ -1,10 +1,8 @@
-package com.repository;
+package com.database.repository;
 
-import com.model.ModbusData;
+import com.database.model.ModbusData;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import javax.crypto.Cipher;
 
 @Repository
 public interface ModbusDataRepository extends JpaRepository<ModbusData, Long> {
